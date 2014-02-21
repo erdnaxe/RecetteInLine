e4ff056b4e818186ef42f056d297df73eec90d23
erdnaxe <alexandre.iooss@sfr.fr>
2014-02-21 15:36:53 +0100

    Initiating composer ! Adding atomik language support and JQuery.

---
+++ app/languages/fr.php
@@ -0,0 +1,13 @@
+<?php
+/* 
+ * French translation
+ * --------------------------
+ * This code is a part of the RecetteInLigne project.
+ * He has been created by erdnaxe.
+ * This project is under GPLv2 license.
+ */
+
+Atomik\Translations::setMessages(array(
+    'hello' => 'bonjour',
+    'how are you?' => 'comment ca va?'
+));
--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
+/* 
+ * Index file
+ * --------------------------
+ * This code is a part of the RecetteInLigne project.
+ * He has been created by erdnaxe.
+ * This project is under GPLv2 license.
+ */
 
 require 'vendor/autoload.php';
-
 Atomik::run();
